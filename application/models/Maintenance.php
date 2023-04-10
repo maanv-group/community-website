84c98a34ed9d9775a7238dca89173d5e3918ecd0
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-04-10 23:59:16 +0530

    Slight Change

--- application/models/Maintenance.php
+++ application/models/Maintenance.php
@@ -1,8 +1,8 @@
 <?php
 class Maintenance extends CI_Model{
-    public function check_n_show($view){
+    public function check_n_show($view, $data){
         if(in_array($view,$this->config->item("offline_views"))){
-            $this->load->view($view);
+            $this->load->view($view, $data);
         } else {
             $this->load->view("status/maintenance");
         }
