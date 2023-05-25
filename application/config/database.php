faab853ad81375df14e404b31ba84e3153609158
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-05-25 17:53:00 +0530

    DB Changes

--- application/config/database.php
+++ application/config/database.php
@@ -73,9 +73,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'production';
 $query_builder = TRUE;
 
-$db['production'] = array(
+$db['development'] = array(
 	'dsn'	=> '',
 	'hostname' => '151.106.116.205',
+	'username' => 'u862955700_admin1',
+	'password' => 'Admin1@skillsurge',
+	'database' => 'u862955700_skillsurge',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+$db['production'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
 	'username' => 'u862955700_admin1',
 	'password' => 'Admin1@skillsurge',
 	'database' => 'u862955700_skillsurge',
