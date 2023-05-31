770a52ac8007abfddb01c2e1289f0efcb863636d
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-05-31 17:31:37 +0530

    Unreserved

--- application/views/pages/landing/initial-01.php
+++ application/views/pages/landing/initial-01.php
@@ -509,17 +509,17 @@
                         </p>
                     </div>
                     <div class="text-content">
-                    <div class="keep-touch">
-                        <div class="title">
-                        <h3>Keep in touch with us.</h3>
-                        <p>We are available at below points to get you resolved for any queries you have.</p>
+                        <div class="keep-touch">
+                            <div class="title">
+                                <h3>Keep in touch with us.</h3>
+                                <p>We are available at below points to get you resolved for any queries you have.</p>
+                            </div>
+                            <div class="chip-set">
+                                <div class="contact-chip"></div>
+                                <div class="contact-chip"></div>
+                                <div class="contact-chip"></div>
+                            </div>
                         </div>
-                        <div class="chip-set">
-                            <div class="contact-chip"></div>
-                            <div class="contact-chip"></div>
-                            <div class="contact-chip"></div>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
@@ -527,7 +527,7 @@
         <section id="map">
             <div class="row justify-content-center">
                 <div class="col-md-11 col-12">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.672643328343!2d72.96364367590803!3d19.16580154914596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b98a9d7a1af9%3A0x954db4c09383f9b3!2sMaanv%20Group!5e0!3m2!1sen!2sin!4v1685508885483!5m2!1sen!2sin" height="500" style="border:0; width:100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.672643328343!2d72.96364367590803!3d19.16580154914596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b98a9d7a1af9%3A0x954db4c09383f9b3!2sMaanv%20Group!5e0!3m2!1sen!2sin!4v1685508885483!5m2!1sen!2sin" height="500" style="border:0; width:100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </section>
@@ -537,7 +537,7 @@
                     <div class="section-title text-center">
                         <h2>Frequently Asked Questions</h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem repudiandae expedita alias veniam deserunt perspiciatis possimus eligendi velit. Cum, mollitia.
+                            This would be the FAQs that you can have a look while
                         </p>
                     </div>
                     <div class="text-content">
@@ -545,36 +545,48 @@
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
                                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                        Accordion Item #1
+                                        What is Collabora?
                                     </button>
                                 </h2>
                                 <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                        Collabora is a vibrant community designed for freelancers from various industries to connect, collaborate, and thrive together. It provides a platform for freelancers to network, share experiences, find exciting opportunities, and unlock their full potential.
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                        Accordion Item #2
+                                        Is Collabora open to freelancers from all industries?
                                     </button>
                                 </h2>
                                 <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                        Absolutely! Collabora welcomes freelancers from diverse industries, including but not limited to design, writing, marketing, programming, photography, consulting, and more. Regardless of your field, you'll find a community of like-minded professionals ready to connect and collaborate with you.
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                        Accordion Item #3
+                                        Is there a fee to join Collabora?
                                     </button>
                                 </h2>
                                 <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                        No, joining Collabora is free! We believe in providing an inclusive community for freelancers to connect and collaborate without any barriers. However, we also offer an elite version called Collabora Pro that provides additional premium features and benefits for freelancers looking to elevate their careers further.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                        How can I make the most out of Collabora?
+                                    </button>
+                                </h2>
+                                <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        To make the most out of Collabora, we encourage you to actively engage with the community. Connect with fellow freelancers, participate in discussions, share your expertise, and collaborate on exciting projects. The more you engage, the more you'll benefit from the wealth of knowledge and opportunities within Collabora.
                                     </div>
                                 </div>
                             </div>
@@ -583,29 +595,6 @@
                 </div>
             </div>
         </section>
-        <p>
-            Join a vibrant network of talented freelancers from around the world. Whether you're a seasoned professional or just starting your freelancing journey, our community is here to support, connect, and empower you.
-
-            Why Choose Our Freelance Community?
-
-            Collaboration Opportunities: Unlock endless possibilities by collaborating with like-minded freelancers across various industries. Connect with individuals who complement your skills and expertise, enabling you to take on larger and more diverse projects.
-
-            Skill Development: Stay ahead of the game with access to a rich pool of resources, educational materials, and expert-led workshops. Sharpen your skills, learn new techniques, and expand your knowledge base through continuous learning and professional development.
-
-            Networking and Connections: Forge valuable connections with freelancers who understand the unique challenges and joys of the freelance lifestyle. Exchange ideas, seek advice, and build a strong professional network that will support your growth and success.
-
-            Project Opportunities: Discover exciting project opportunities that match your expertise and interests. Our community is a hub for clients seeking talented freelancers, providing you with a platform to showcase your portfolio, receive project invitations, and secure rewarding gigs.
-
-            Support and Mentoring: Tackle obstacles and navigate the freelance landscape with the guidance of experienced mentors. Benefit from their wisdom and practical insights, gaining the confidence and knowledge to overcome challenges and excel in your freelance career.
-
-            Community Events and Meetups: Participate in virtual and in-person events tailored to freelancers. Attend webinars, conferences, and networking meetups where you can engage with industry leaders, expand your professional network, and stay updated on the latest trends and opportunities.
-
-            Resources and Tools: Access a curated collection of freelancing resources, including contract templates, invoicing tools, and time management applications. Streamline your workflow, enhance your productivity, and ensure a smooth freelance experience.
-
-            Join the Freelance Community today and embark on a journey of professional growth, personal fulfillment, and endless possibilities. Together, we can redefine the future of freelance work!
-
-            Sign up now and unlock the full potential of your freelance career. Let's thrive together in this dynamic and supportive community.
-        </p>
     </main>
     <?php $this->load->view("components/footer") ?>
 </body>
