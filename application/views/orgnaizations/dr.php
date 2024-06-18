f512ee6031f83ef363a9e67e40fe94ce84fa09fd
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-06-18 18:50:40 +0530

    Digital Room Page

--- application/views/orgnaizations/dr.php
+++ application/views/orgnaizations/dr.php
@@ -3,15 +3,23 @@
 
 <head>
 	<?php $this->load->view("components/head") ?>
+
+	<!-- Plugin: LightGallery -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/css/lightgallery-bundle.min.css" integrity="sha512-nUqPe0+ak577sKSMThGcKJauRI7ENhKC2FQAOOmdyCYSrUh0GnwLsZNYqwilpMmplN+3nO3zso8CWUgu33BDag==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/lightgallery.min.js" integrity="sha512-jEJ0OA9fwz5wUn6rVfGhAXiiCSGrjYCwtQRUwI/wRGEuWRZxrnxoeDoNc+Pnhx8qwKVHs2BRQrVR9RE6T4UHBg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/lightgallery.umd.min.js" integrity="sha512-VOQBxCIgNssJrB8+irZF7L8MvfpAshegc36C3H5QD7vmibXM4uCNaqJIaSNatD2z2ZQQJSx0k+q+m+xsSPp4Xw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/plugins/thumbnail/lg-thumbnail.umd.min.js" integrity="sha512-dc8xJSGs0ib9uo0fLT/v4wp2LG7+4OSzc+UpFiIKiv6QP/e4hZH/S8manUCTtO3tNVGzcje8uJjSdL+NH29blQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/plugins/zoom/lg-zoom.umd.min.js" integrity="sha512-OUF2jbRheQR5yXPCvXN71udWa5cvwPf+shcXM+5GrW1vtNurTn7az8LCP3hS50gm17ULXdh3cdkhiPa0Qqyczw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 	<link rel="stylesheet" href="<?= base_url() ?>assets/css/company-profile.min.css">
 	<title><?= $page['title'] ?></title>
 </head>
-<main>
+<main class="company-profile">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-3">
 				<div class="card">
-					<div class="card-body text-center">
+					<div class="card-body">
 						<div class="row g-3">
 							<div class="col-12">
 								<div class="brand-logo">
@@ -20,7 +28,7 @@
 									</div>
 								</div>
 							</div>
-							<div class="col-12">
+							<div class="col-12 text-center">
 								<div class="company-brief">
 									<h4>Digital Room</h4>
 									<p class="text-muted">Since 2019.</p>
@@ -44,8 +52,177 @@
 									</ul>
 								</div>
 							</div>
+							<hr>
+							<div class="col-12">
+								<div class="profile-title mb-3">
+									<h5>Profile Overview</h5>
+								</div>
+								<div class="row gx-0 gy-2">
+									<div class="col-12">
+										<strong class="text-uppercase"><small>Owner Name</small></strong><br>
+										Divya Tamhane
+									</div>
+									<div class="col-12">
+										<strong class="text-uppercase"><small>Location</small></strong><br>
+										Mumbai, Maharashtra. India
+									</div>
+									<div class="col-12">
+										<strong class="text-uppercase"><small>Established</small></strong><br>
+										February 2019
+									</div>
+									<div class="col-12">
+										<a href="" class="btn btn-theme-01 w-100">Contact</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-12">
+								<div class="profile-title mb-3">
+									<h5>Location</h5>
+								</div>
+								<div class="">
+									<iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.6727381178794!2d72.96132828594276!3d19.165797402898875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b98a9d7a1af9%3A0x954db4c09383f9b3!2sMaanv%20Group!5e0!3m2!1sen!2sin!4v1718691794894!5m2!1sen!2sin" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+								</div>
+							</div>
 						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-9">
+				<div class="card">
+					<div class="card-body">
+						<ul class="nav nav-tabs" id="myTab" role="tablist">
+							<li class="nav-item" role="presentation">
+								<button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Home</button>
+							</li>
+							<li class="nav-item" role="presentation">
+								<button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Profile</button>
+							</li>
+							<li class="nav-item" role="presentation">
+								<button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Contact</button>
+							</li>
+							<li class="nav-item" role="presentation">
+								<button class="nav-link" id="disabled-tab" data-bs-toggle="tab" data-bs-target="#disabled-tab-pane" type="button" role="tab" aria-controls="disabled-tab-pane" aria-selected="false" disabled>Disabled</button>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="tab-content" id="myTabContent">
+					<div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+						<div class="card">
+							<div class="card-body">
+								<div class="row g-3">
+									<div class="col-12">
+										<div class="mb-3">
+											<h5>About Company</h5>
+										</div>
+										<div class="">
+											<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque quo in nesciunt officia, inventore vel voluptatum saepe adipisci et debitis unde voluptates, maiores laudantium repellendus provident excepturi labore. Officia doloremque velit, commodi voluptate assumenda saepe laudantium minus excepturi! Eligendi voluptatum deleniti minima officiis deserunt reiciendis at optio, dolorem commodi accusamus, accusantium numquam consequuntur aliquid amet cupiditate tempore. Voluptate tempore, commodi fugit consequatur nihil, vero voluptates quo harum eos modi necessitatibus earum a sit distinctio consequuntur expedita, non error magnam. Soluta asperiores, possimus iusto optio molestiae veniam, distinctio harum nihil dolorem fuga vitae aut odio! Ex veritatis soluta perspiciatis quod quibusdam?</p>
+											<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum debitis delectus maiores totam, quia deleniti quo corporis porro assumenda ullam.</p>
+											<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nisi consequatur impedit natus ex dolore numquam aut? Quae consectetur adipisci distinctio enim aspernatur in. Repudiandae culpa, qui voluptates exercitationem libero, esse neque sed illum repellat nostrum veniam consequuntur, blanditiis nihil.</p>
+										</div>
+									</div>
+									<div class="col-12">
+										<div class="mb-3">
+											<h5>Gallery</h5>
+										</div>
+										<div class="row" id="lightgallery">
+											<a class="col-lg-4 col-md-6 col-12" href="https://placehold.co/1500x2125/jpg">
+												<picture>
+													<source srcset="https://placehold.co/500x300/webp" type="image/webp">
+													<source srcset="https://placehold.co/500x300/jpg" type="image/jpg">
+													<img src="https://placehold.co/500x300/jpg" alt="" class="w-100">
+												</picture>
+											</a>
+											<a class="col-lg-4 col-md-6 col-12" href="https://placehold.co/1500x2125/jpg">
+												<picture>
+													<source srcset="https://placehold.co/500x300/webp" type="image/webp">
+													<source srcset="https://placehold.co/500x300/jpg" type="image/jpg">
+													<img src="https://placehold.co/500x300/jpg" alt="" class="w-100">
+												</picture>
+											</a>
+										</div>
+										<script type="text/javascript">
+											lightGallery(document.getElementById('lightgallery'), {
+												plugins: [lgZoom, lgThumbnail],
+												speed: 500,
+											});
+										</script>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+						<div class="card">
+							<div class="card-body">
+								<div class="row g-3">
+									<div class="col-12">
+										<div class="mb-3">
+											<h5>About Company</h5>
+										</div>
+										<div class="">
+											<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque quo in nesciunt officia, inventore vel voluptatum saepe adipisci et debitis unde voluptates, maiores laudantium repellendus provident excepturi labore. Officia doloremque velit, commodi voluptate assumenda saepe laudantium minus excepturi! Eligendi voluptatum deleniti minima officiis deserunt reiciendis at optio, dolorem commodi accusamus, accusantium numquam consequuntur aliquid amet cupiditate tempore. Voluptate tempore, commodi fugit consequatur nihil, vero voluptates quo harum eos modi necessitatibus earum a sit distinctio consequuntur expedita, non error magnam. Soluta asperiores, possimus iusto optio molestiae veniam, distinctio harum nihil dolorem fuga vitae aut odio! Ex veritatis soluta perspiciatis quod quibusdam?</p>
+											<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum debitis delectus maiores totam, quia deleniti quo corporis porro assumenda ullam.</p>
+											<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nisi consequatur impedit natus ex dolore numquam aut? Quae consectetur adipisci distinctio enim aspernatur in. Repudiandae culpa, qui voluptates exercitationem libero, esse neque sed illum repellat nostrum veniam consequuntur, blanditiis nihil.</p>
+										</div>
+									</div>
+									<div class="col-12">
+										<div class="mb-3">
+											<h5>Gallery</h5>
+										</div>
 
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
+						<div class="card">
+							<div class="card-body">
+								<div class="row g-3">
+									<div class="col-12">
+										<div class="mb-3">
+											<h5>About Company</h5>
+										</div>
+										<div class="">
+											<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque quo in nesciunt officia, inventore vel voluptatum saepe adipisci et debitis unde voluptates, maiores laudantium repellendus provident excepturi labore. Officia doloremque velit, commodi voluptate assumenda saepe laudantium minus excepturi! Eligendi voluptatum deleniti minima officiis deserunt reiciendis at optio, dolorem commodi accusamus, accusantium numquam consequuntur aliquid amet cupiditate tempore. Voluptate tempore, commodi fugit consequatur nihil, vero voluptates quo harum eos modi necessitatibus earum a sit distinctio consequuntur expedita, non error magnam. Soluta asperiores, possimus iusto optio molestiae veniam, distinctio harum nihil dolorem fuga vitae aut odio! Ex veritatis soluta perspiciatis quod quibusdam?</p>
+											<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum debitis delectus maiores totam, quia deleniti quo corporis porro assumenda ullam.</p>
+											<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nisi consequatur impedit natus ex dolore numquam aut? Quae consectetur adipisci distinctio enim aspernatur in. Repudiandae culpa, qui voluptates exercitationem libero, esse neque sed illum repellat nostrum veniam consequuntur, blanditiis nihil.</p>
+										</div>
+									</div>
+									<div class="col-12">
+										<div class="mb-3">
+											<h5>Gallery</h5>
+										</div>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">
+						<div class="card">
+							<div class="card-body">
+								<div class="row g-3">
+									<div class="col-12">
+										<div class="mb-3">
+											<h5>About Company</h5>
+										</div>
+										<div class="">
+											<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque quo in nesciunt officia, inventore vel voluptatum saepe adipisci et debitis unde voluptates, maiores laudantium repellendus provident excepturi labore. Officia doloremque velit, commodi voluptate assumenda saepe laudantium minus excepturi! Eligendi voluptatum deleniti minima officiis deserunt reiciendis at optio, dolorem commodi accusamus, accusantium numquam consequuntur aliquid amet cupiditate tempore. Voluptate tempore, commodi fugit consequatur nihil, vero voluptates quo harum eos modi necessitatibus earum a sit distinctio consequuntur expedita, non error magnam. Soluta asperiores, possimus iusto optio molestiae veniam, distinctio harum nihil dolorem fuga vitae aut odio! Ex veritatis soluta perspiciatis quod quibusdam?</p>
+											<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum debitis delectus maiores totam, quia deleniti quo corporis porro assumenda ullam.</p>
+											<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nisi consequatur impedit natus ex dolore numquam aut? Quae consectetur adipisci distinctio enim aspernatur in. Repudiandae culpa, qui voluptates exercitationem libero, esse neque sed illum repellat nostrum veniam consequuntur, blanditiis nihil.</p>
+										</div>
+									</div>
+									<div class="col-12">
+										<div class="mb-3">
+											<h5>Gallery</h5>
+										</div>
+
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
